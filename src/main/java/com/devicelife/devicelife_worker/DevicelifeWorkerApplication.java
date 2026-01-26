26e8342ea1d76409a4027bca10100a82d347a0c2
weeeeestern <liz0824@naver.com>
2026-01-26 16:18:38 +0900

    feat: init worker project with aws & sqs config

---
+++ src/main/java/com/devicelife/devicelife_worker/DevicelifeWorkerApplication.java
@@ -0,0 +1,13 @@
+package com.devicelife.devicelife_worker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevicelifeWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevicelifeWorkerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devicelife/devicelife_worker/DevicelifeWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devicelife.devicelife_worker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevicelifeWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
